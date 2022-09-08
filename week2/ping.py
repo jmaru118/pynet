7441f788782477aa1fe1e213eeadedbce6a4b4ba
Jonathan Martinez <jmartinez@archive.org>
2022-09-07 20:12:46 -0700

    week 2

---
+++ week2/ping.py
@@ -0,0 +1,30 @@
+from netmiko import ConnectHandler 
+from getpass import getpass 
+
+cisco4 = {
+    "host":  "cisco4.lasthop.io", 
+    "username": "pyclass", 
+    "password": getpass(), 
+    "device_type": "cisco_ios",
+}
+
+# send_command_timing with extended ping
+net_connect = ConnectHandler(**cisco4)
+output = ""
+commands = [ "ping", '\n', '8.8.8.8', '\n', '\n', '\n', '\n', '\n' ]
+for cmd in commands:
+    output += net_connect.send_command_timing(cmd, strip_command = False,
+                                                    strip_prompt = False)
+
+print("#### SEND_COMMAND_TIMING IMPLEMENTATION ####")
+print(output)
+
+# send_command with extended ping
+output = ""
+commands = [ "ping", '\n', '8.8.8.8', '\n', '\n', '\n', '\n', '\n' ]
+for cmd in commands:
+    output += net_connect.send_command(cmd, expect_string = r':', 
+                            strip_command = False, strip_prompt = False)
+
+print("\n\n#### SEND_COMMAND IMPLEMENTATION ####")
+print(output)
---
+++ week2/show_neighbors.py
@@ -0,0 +1,35 @@
+from netmiko import ConnectHandler 
+from getpass import getpass 
+import time
+
+device = {
+    "host":  "nxos2.lasthop.io", 
+    "username": "pyclass", 
+    "password": getpass(), 
+    "device_type": "cisco_nxos",
+    "global_delay_factor": 2,
+    "fast_cli": "False"
+}
+
+# No-delay implementation
+net_connect = ConnectHandler(**device)
+output = ""
+cmd = "show lldp neighbors detail"
+start = time.time()
+output += net_connect.send_command_timing(cmd, strip_command = False,
+                                                    strip_prompt = False)
+end = time.time()
+elapsed_time = end - start
+print(output)
+print("\nElapsed Time: " + str(elapsed_time))
+
+# delay implementation
+output = ""
+cmd = "show lldp neighbors detail"
+start = time.time()
+output += net_connect.send_command_timing(cmd, delay_factor = 8, 
+                            strip_command = False, strip_prompt = False)
+end = time.time()
+elapsed_time = end - start
+print(output)
+print("\nElapsed Time With Delay: " + str(elapsed_time))
